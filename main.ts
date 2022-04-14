f981966ce1a8d65a2564216225af6e07b798a1e1
karlpederolesen <96655854+karlpederolesen@users.noreply.github.com>
2022-04-14 20:33:57 +0200

    Opdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let sonar2 = 0
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+RingbitCar.forward()
 basic.forever(function () {
-	
+    sonar2 = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0)
+    if (sonar2 < 20 && sonar2 != 0) {
+        RingbitCar.freestyle(0, 100)
+        basic.pause(400)
+    } else {
+        RingbitCar.forward()
+    }
 })
